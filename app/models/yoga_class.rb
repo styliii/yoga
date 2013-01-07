abe0238be1f63f521a30712fbe4c0be16f95df4a
Robert Beene <rbeene@teladoc.com>
2013-01-07 08:45:12 -0500

    Update class for day method and insert methods
    todays classes is really just a shortcut to get to classes for a given
    day. Created a class method that handles querying for this. todays
    classes leverages this method and simply passes current time.
    
    Insert new method now takes an array. If for whatever reason an
    exception is thrown during processing, rescue to "next"

--- app/models/yoga_class.rb
+++ app/models/yoga_class.rb
@@ -5,21 +5,32 @@ class YogaClass < ActiveRecord::Base
 
   validates :class_date_time, :presence => true, :uniqueness => { :scope => :teacher_id, :message => "duplicate yoga class"}
 
+
+  def self.classes_for_day(day)
+    # note that for this to work in multiple time zones, you should store in UTC and do handle conversions based on time zone of
+    # the user viewing a particular page - disregarding for now
+    where("class_date_time >= ? AND class_date_time < ?", day.beginning_of_day, day.end_of_day)
+  end
+
   def self.todays_classes
-    where("class_date_time >= :start_date AND class_date_time <= :end_date",
-    {:start_date => DateTime.now, :end_date => DateTime.now.end_of_day}).order("class_date_time")
+    self.classes_for_day(Time.now)
   end
 
   def self.todays_fav_classes
     YogaClass.todays_classes.collect{|yc| yc if yc.visible? }.compact
   end
 
-  def self.insert_new(details = {})
-    studio_id = Studio.find_or_create_by_name(details[:studio]).id
-    teacher_id = Teacher.find_or_create_by_first_name(first_name: details[:teachers_first_name], last_name: details[:teachers_last_name]).id
-    class_date_time = Chronic.parse(details[:class_date_time])
-
-    YogaClass.create(studio_id: studio_id, teacher_id: teacher_id, class_date_time: class_date_time )
+  def self.insert_new(details)
+    details.each do |detail|
+      begin
+        studio_id       = Studio.find_or_create_by_name(details[:studio]).id
+        teacher_id      = Teacher.find_or_create_by_first_name(first_name: details[:teachers_first_name], last_name: details[:teachers_last_name]).id
+        class_date_time = Chronic.parse(details[:class_date_time])
+        YogaClass.create(studio_id: studio_id, teacher_id: teacher_id, class_date_time: class_date_time )
+      rescue
+        next
+      end
+    end
   end
 
   def visible?
