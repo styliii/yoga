f000e668b7ee32631d66f11cc033527cde587af2
Li Ouyang <li.ouyang@gmail.com>
2013-01-09 02:32:37 -0500

    created helper method to get to next days classes

--- app/helpers/yoga_classes_helper.rb
+++ app/helpers/yoga_classes_helper.rb
@@ -1,6 +1,10 @@
 module YogaClassesHelper
-  def link_to_tomorrows_classes(description)
-    date = DateTime.now.tomorrow
-    link_to description, "yoga_classes/#{date.strftime("%y")}/#{date.strftime("%m")}/#{date.strftime("%d")}"
+  def link_to_next_days_classes(description)
+    if params[:day]
+      date = Chronic.parse("#{params[:month]}/#{params[:day]}/#{params[:year]}").tomorrow
+    else
+      date = DateTime.now.tomorrow
+    end
+    link_to description, "/yoga_classes/#{date.strftime("%y")}/#{date.strftime("%m")}/#{date.strftime("%d")}"
   end
 end
