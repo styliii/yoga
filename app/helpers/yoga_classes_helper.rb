9468027febcecc39dde5d9fe780ad32ae96a8111
Li Ouyang <li.ouyang@gmail.com>
2013-01-09 02:20:30 -0500

    quick link to tomorrow's classes unformatted

--- app/helpers/yoga_classes_helper.rb
+++ app/helpers/yoga_classes_helper.rb
@@ -1,2 +1,6 @@
 module YogaClassesHelper
+  def link_to_tomorrows_classes(description)
+    date = DateTime.now.tomorrow
+    link_to description, "yoga_classes/#{date.strftime("%y")}/#{date.strftime("%m")}/#{date.strftime("%d")}"
+  end
 end
