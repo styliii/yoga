c5a94be56f8d431aa7301bd0df44a7e4d2f023ef
Li Ouyang <li.ouyang@gmail.com>
2013-05-22 17:00:04 -0400

    argh

--- config/application.rb
+++ config/application.rb
@@ -54,7 +54,8 @@ module MyYogaPractice
     config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
-    config.assets.enabled = false
+    config.assets.enabled = true
+    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
